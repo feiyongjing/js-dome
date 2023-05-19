3629f15a312cf2961f3655cc6de47294ff210c46
feiyogjing <feiyongjing@cloudgrow.cn>
2023-05-19 18:25:16 +0800

    vue3-toRef和toRefs

---
+++ vue3-cli/src-toRef和toRefs简写/App.vue
@@ -0,0 +1,33 @@
+<template>
+<button @click="isShowDemo = !isShowDemo">切换隐藏/显示</button>
+  <Demo v-if="isShowDemo"/>
+</template>
+
+<script>
+import {ref} from "vue";
+import Demo from "./components/Demo.vue";
+
+export default {
+  name: "App",
+  components: { Demo },
+
+  setup() {
+    let isShowDemo=ref(true)
+
+    return {
+      isShowDemo
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
