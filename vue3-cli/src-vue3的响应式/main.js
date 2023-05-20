33a2a9949269b931032593de449a4828e0298743
feiyongjing <2059326740@qq.com>
2023-05-20 22:13:41 +0800

    vue3其他

--- vue3-cli/src-vue3的响应式/main.js
+++ vue3-cli/src-vue3的响应式/main.js
@@ -3,4 +3,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // 调用工厂函数createApp生成应用实例对象（类似于vue2的vm,但是比vm“轻”），并与标签id属性是app的标签绑定挂载
-createApp(App).mount('#app')
+const app=createApp(App)
+app.mount('#app')
+
+
+// 需要注意的是vue2中的Vue.xxx 等配置全部都转移到了createApp(App)返回的应用实例对象上了
+// 例如 Vue.use()  在vue3中是createApp(App).use()
