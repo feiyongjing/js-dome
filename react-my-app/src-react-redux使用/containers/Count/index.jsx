5dd0fbf84cebd5d83ece010063d2eb22c901152c
feiyongjing <2059326740@qq.com>
2024-08-12 22:06:45 +0800

    react-redux使用

---
+++ react-my-app/src-react-redux使用/containers/Count/index.jsx
@@ -0,0 +1,38 @@
+// 引入react-redux的connect用于自定义的Count UI组件和redux的连接
+import { connect } from 'react-redux'
+
+// 引入自定义的action对象构建函数
+import { createIncrementAction, createDecrementAction, createIncrementAsyncAction } from '../../redux/count/create_count_action'
+
+// 引入自定义的Count UI组件
+import CountUI from '../../components/Count/Count'
+
+// 参数是父组件（App组件）传递的store对象调用getState()获取redux中的数据
+// 返回值作为向UI组件（CountUI）传递的 props 数据
+function mapStateToProps(state) {
+    return {
+        count: state
+    }
+}
+
+// 参数是父组件（App组件）传递的store中的dispatch函数，可以用于修改redux中的数据
+// 返回值作为向UI组件（CountUI）传递的 props 函数
+// mapDispatchToProps编写方式一：如下编写函数
+// function mapDispatchToProps(dispatch) {
+//     return {
+//         dispatch: (action)=> dispatch(action)
+//     }
+// }
+
+// mapDispatchToProps编写方式二：如下编写对象
+// 本质是使用key，代理了action对象的创建和dispatch函数调用修改redux中的数据
+// 所以使用key时传递的参数与action创建时传递的参数一致
+const mapDispatchToProps = {
+    jia: createIncrementAction,
+    jian: createDecrementAction,
+    jiaAsync: createIncrementAsyncAction
+}
+
+
+// 创建一个 Count 容器组件，包裹CountUI 组件，并且向它传递props数据和函数
+export default connect(mapStateToProps, mapDispatchToProps)(CountUI)
---
+++ react-my-app/src-react-redux使用/redux/count/count_reducer.js
@@ -0,0 +1,21 @@
+import {INCREMENT, DECREMENT} from './create_count_action' // 引入自定义的action操作常量
+
+/**
+ * reducer 进行实际的数据修改
+ * @param {*} oldState 表示之前的state数据，如果是第一次或者不传递就是undefined，会使用默认参数
+ * @param {*} action 动作对象，内部有type和data表示需要进行的操作类型和传递的数据
+ * @returns 新的state数据
+ */
+const initState = 0 // 初始化的数据
+export default function count_reducer(oldState=initState, action) {
+    const { type, data } = action
+    switch (type) {
+        case INCREMENT:
+            return oldState + data
+        case DECREMENT:
+            return oldState - data
+        default:
+            return oldState
+    }
+
+}
